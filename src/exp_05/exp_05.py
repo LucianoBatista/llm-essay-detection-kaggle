654da670d261740295c65a162a689f5c56dc7415
Luba <luciano.eq@outlook.com>
2023-12-11 04:24:50 -0300

    :test_tube: experiments.

--- src/exp_05/exp_05.py
+++ src/exp_05/exp_05.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
 
 import polars as pl
+from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer
 
@@ -58,6 +62,8 @@ class DataPreprocessing:
             fraction=1, shuffle=True, seed=42
         )
 
+        return self.data_train.select(["text"]), self.data_train.select(["generated"])
+
     def split_data(self):
         """
         Split data into train and test
@@ -72,15 +78,12 @@ class DataPreprocessing:
         return X_train, X_test, y_train, y_test
 
 
-class Exp04:
-    def __init__(self, X_train, X_test, y_train, y_test, config: Config):
-        self.X_train = X_train
-        self.X_test = X_test
+class Exp05:
+    def __init__(self, data_train, y_train, config: Config):
+        self.data_train = data_train
         self.y_train = y_train
-        self.y_test = y_test
-        # adjust tokenizer path to change base on wich environent we are
-        # kaggle or local
         self.tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_path)
+        # self.tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
         self.tfidf = None
         self.clf = None
         self.predictions = None
@@ -89,59 +92,74 @@ class Exp04:
         """
         Train tokenizer on X_train
         """
-        train_texts = self.X_train.select("text").to_numpy().flatten().tolist()
-        self.tokenizer = self.tokenizer.train_new_from_iterator(train_texts, 52000)
+        train_texts = self.data_train.to_numpy().flatten().tolist()
+        self.tokenizer = self.tokenizer.train_new_from_iterator(train_texts, 32000)
 
     def tokenize(self):
         """
         Tokenize text
         """
-        train_texts = self.X_train.select("text").to_numpy().flatten().tolist()
-        test_texts = self.X_test.select("text").to_numpy().flatten().tolist()
+        train_texts = self.data_train.to_numpy().flatten().tolist()
 
         train_tokenized = []
         for text in tqdm(train_texts):
             text_tokenized = self.tokenizer.tokenize(text)
             train_tokenized.append(" ".join(text_tokenized))
 
-        test_tokenized = []
-        for text in tqdm(test_texts):
-            text_tokenized = self.tokenizer.tokenize(text)
-            test_tokenized.append(" ".join(text_tokenized))
-
-        self.X_train = train_tokenized
-        self.X_test = test_tokenized
+        self.data_train = train_tokenized
 
     def train_tfidf(self):
         """
         Train tfidf vectorizer
         """
         self.tfidf = TfidfVectorizer(
+            ngram_range=(3, 5),
             stop_words="english",
             lowercase=False,
             sublinear_tf=True,
             strip_accents="unicode",
         )
-        self.tfidf.fit(self.X_train)
+        self.tfidf.fit(self.data_train)
 
     def vectorize(self):
-        self.X_train = self.tfidf.transform(self.X_train)
-        self.X_test = self.tfidf.transform(self.X_test)
+        self.data_train = self.tfidf.transform(self.data_train)
 
-    def exp04(self):
+    def exp05_w_cv(self):
         """
         Train and evaluate model
         """
-        clf = MultinomialNB()
-        clf.fit(self.X_train, self.y_train)
-        self.predictions = clf.predict_proba(self.X_test)[:, 1]
-        self.clf = clf
+        clf = MultinomialNB(alpha=0.2)
+        models = [
+            (
+                "SGD",
+                SGDClassifier(
+                    max_iter=8000, tol=1e-4, loss="modified_huber", n_jobs=10
+                ),
+            ),
+            ("Logistic Regression", LogisticRegression()),
+            # ("Gradient Boosting", HistGradientBoostingClassifier()),
+            ("Multinomial Naive Bayes", clf),
+            # ("Naive Bayes", clf),
+        ]
 
-        # calculate roc auc
-        roc_auc = roc_auc_score(
-            self.y_test.to_numpy().flatten().tolist(), self.predictions
-        )
-        print(f"ROC AUC: {roc_auc}")
+        for name, model in models:
+            scores = cross_val_score(
+                model,
+                self.data_train,
+                self.y_train.to_numpy().flatten().tolist(),
+                cv=10,
+                scoring="roc_auc",
+            )
+            print(f"Cross validation scores for {name}: {scores}")
+            print(f"Mean cross validation score for {name}: {scores.mean()}")
+
+    def exp05_model(self):
+        """
+        Train and evaluate model
+        """
+        clf = LogisticRegression()
+        clf.fit(self.data_train, self.y_train)
+        self.clf = clf
 
     def submission(self, real_test):
         """
@@ -164,18 +182,36 @@ class Exp04:
         )
         df.write_csv("submission_tfidf.csv")
 
+    def evaluate_model(self, X_test, y_test):
+        """
+        Evaluate model
+        """
+        train_texts = X_test.to_numpy().flatten().tolist()
+
+        test_tokenized = []
+        for text in tqdm(train_texts):
+            text_tokenized = self.tokenizer.tokenize(text)
+            test_tokenized.append(" ".join(text_tokenized))
+
+        X_test = test_tokenized
+        X_test = self.tfidf.transform(X_test)
+        predictions = self.clf.predict_proba(X_test)[:, 1]
+        print(f"ROC AUC score: {roc_auc_score(y_test, predictions)}")
+
 
 if __name__ == "__main__":
     config = Config()
     data_preprocessing = DataPreprocessing(config)
-    data_preprocessing.pre_processing()
+    data_train, y_train_full = data_preprocessing.pre_processing()
     X_train, X_test, y_train, y_test = data_preprocessing.split_data()
-    exp04 = Exp04(X_train, X_test, y_train, y_test, config)
-    exp04.train_tokenizer()
-    exp04.tokenize()
-    exp04.train_tfidf()
-    exp04.vectorize()
-    exp04.exp04()
+
+    exp05 = Exp05(data_train, y_train_full, config)
+    exp05.train_tokenizer()
+    exp05.tokenize()
+    exp05.train_tfidf()
+    exp05.vectorize()
+    exp05.exp05_w_cv()
+    # exp05.evaluate_model(X_test, y_test)
 
     if KAGGLE:
-        exp04.submission(data_preprocessing.data_test)
+        exp05.submission(data_preprocessing.data_test)
